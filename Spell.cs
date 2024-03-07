92cc530ce88fbc32194924c70401e364004e5d7a
KYGAS <45013496+KYGAS@users.noreply.github.com>
2024-03-07 15:24:41 +0100

    Spell Management

---
+++ Spell.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Spell", menuName = "Spell")]
+public class Spell : ScriptableObject
+{
+    public enum SpellType
+    {
+        Fire,
+        Water,
+        Air,
+        Earth,
+        None
+    }
+
+    public enum SpellConnector
+    {
+        And,
+        Then,
+        None
+    }
+
+    public SpellType spellType;
+    public SpellConnector connector;
+    public SpellType secondarySpellType; 
+
+    public float power = 10.0f;
+    public float range = 5.0f;
+    public float castTime = 1.0f;
+    public GameObject spellEffect;
+
+}
---
+++ SpellManager.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SpellManager : MonoBehaviour
+{
+    private Player playerScript;
+
+    private Spell.SpellType primarySpellType;
+    private Spell.SpellConnector connector;
+    private Spell.SpellType secondarySpellType;
+    private SpellHolder holder;
+    private Dictionary<string, Spell> spellDictionary;
+
+    [SerializeField] private float actionLock = 0.500f;
+    private float lastAction = 0;
+
+    void Awake()
+    {
+        holder = GetComponent<SpellHolder>();
+        playerScript = GetComponent<Player>();
+        spellDictionary = new Dictionary<string, Spell>();
+    }
+
+    private void Start()
+    {
+        // Na primer: "Fire_And_Air"
+        Spell[] allSpells = Resources.LoadAll<Spell>("Assets/_Scripts/Spells/_SpellBook"); 
+        foreach (Spell spell in allSpells)
+        {
+            string key = spell.spellType.ToString();
+            if (spell.connector != Spell.SpellConnector.None)
+            {
+                key += "_" + spell.connector.ToString() + "_" + spell.secondarySpellType.ToString();
+            }
+            spellDictionary.Add(key, spell);
+        }
+    }
+
+    private bool CheckAction()
+    {
+        Debug.Log("Check : " + Time.time);
+        if (lastAction + actionLock < Time.time)
+        {
+            Debug.Log("Pass : " + Time.time);
+            lastAction = Time.time;
+            return true;
+        }
+        return false;
+    }
+
+    public void SetSpellType(Spell.SpellType spellType)
+    {
+        if (!CheckAction()) return;
+        if (connector != Spell.SpellConnector.None ) secondarySpellType = spellType;
+        else primarySpellType = spellType;
+
+    }
+    public void SetConnectorType(Spell.SpellConnector spellConnector)
+    {
+        if (!CheckAction()) return;
+        if (secondarySpellType != Spell.SpellType.None) return;
+        connector = spellConnector;
+    }
+
+    public void CraftSpell()
+    {
+        if (!CheckAction()) return;
+        if (primarySpellType == Spell.SpellType.None) return;
+        string key = primarySpellType.ToString();
+        if (connector == Spell.SpellConnector.None)
+        {
+            if(secondarySpellType == Spell.SpellType.None) return;
+            key += "_" + connector.ToString() + "_" + secondarySpellType.ToString();
+        }
+
+        if (spellDictionary.TryGetValue(key, out Spell combinedSpell))
+        {
+            holder.spell = combinedSpell;
+        }else
+        {
+            holder.spell = null;
+        }
+        ResetSpellState();
+        Invoke(nameof(CastSpell), 1);
+    }
+    public void CancelSpell()
+    {
+        if (!CheckAction()) return;
+        ResetSpellState();
+    }
+    private void ResetSpellState()
+    {
+        primarySpellType = Spell.SpellType.None;
+        connector = Spell.SpellConnector.None;
+        secondarySpellType = Spell.SpellType.None;
+    }
+    public void CastSpell()
+    {
+        if (!CheckAction()) return;
+        if (holder.spell == null) throw new System.Exception("Spell type missing.");
+        Debug.Log("Casting : " + holder.spell.name);
+        Instantiate(holder.spell, transform.position, Quaternion.identity) ;
+    }
+
+}

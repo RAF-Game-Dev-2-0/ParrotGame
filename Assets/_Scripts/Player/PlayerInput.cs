1ec521bb6ee2bb51170a4fa2a598173e16deb038
KYGAS <45013496+KYGAS@users.noreply.github.com>
2024-03-07 16:01:47 +0100

    Add spells to player input

--- Assets/_Scripts/Player/PlayerInput.cs
+++ Assets/_Scripts/Player/PlayerInput.cs
@@ -1,22 +1,70 @@
+using System;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour {
 
     private Player playerScript;
+    private SpellManager spellManager;
 
-    void Awake(){
+    [SerializeField] public KeyCode fireSpellKeyCode = KeyCode.Alpha1;
+    [SerializeField] public KeyCode airSpellKeyCode = KeyCode.Alpha2;
+    [SerializeField] public KeyCode earthSpellKeyCode = KeyCode.Alpha3;
+    [SerializeField] public KeyCode waterSpellKeyCode = KeyCode.Alpha4;
+    [SerializeField] public KeyCode andConnectorKeyCode = KeyCode.Q;
+    [SerializeField] public KeyCode thenConnectorKeyCode = KeyCode.E;
+    [SerializeField] public KeyCode launchSpellKeyCode = KeyCode.Mouse0;
+    [SerializeField] public KeyCode cancelSpellKeyCode = KeyCode.Mouse1;
+
+    void Awake()
+    {
         playerScript = GetComponent<Player>();
+        spellManager = GetComponent<SpellManager>();
     }
 
     
     void Update(){
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)){
-            playerScript.FireAbility();
+        HandleSpellInput();
+    }
+
+    private void HandleSpellInput()
+    {
+        Debug.Log("Hello spell input");
+        if (Input.GetKeyDown(fireSpellKeyCode))
+        {
+            spellManager.SetSpellType(Spell.SpellType.Fire);
         }
+        if (Input.GetKeyDown(airSpellKeyCode))
+        {
+            spellManager.SetSpellType(Spell.SpellType.Air);
+        }
+        if (Input.GetKeyDown(earthSpellKeyCode))
+        {
+            spellManager.SetSpellType(Spell.SpellType.Earth);
+        }
+        if (Input.GetKeyDown(waterSpellKeyCode))
+        {
+            spellManager.SetSpellType(Spell.SpellType.Water);
+        }
+        if (Input.GetKeyDown(andConnectorKeyCode))
+        {
+            spellManager.SetConnectorType(Spell.SpellConnector.And);
+        }
+        if (Input.GetKeyDown(thenConnectorKeyCode))
+        {
+            spellManager.SetConnectorType(Spell.SpellConnector.Then);
+        }
+        if (Input.GetKeyDown(launchSpellKeyCode))
+        {
+            spellManager.CraftSpell();
+        }
+        if (Input.GetKeyDown(cancelSpellKeyCode))
+        {
+            spellManager.CancelSpell();
+        }
+
     }
 
     public Vector2 GetMovementVectorNormalized(){
-
         Vector2 inputVector = new Vector2(0, 0);
 
         if (Input.GetKey(KeyCode.W)){
